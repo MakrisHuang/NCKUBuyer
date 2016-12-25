fd744f61dde025f257e18c4fcdfe1354c3d16383
Makris Huang <vallwesture@gmail.com>
2016-12-26 01:27:23 +0800

    configure route.js and db.js

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 var express = require('express');
 var app = express();
 
+// app initialization
 app.use(express.static('app'));
 app.use(express.static('node_modules'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.get('/', function(request, response){
-	response.sendFile(__dirname + '/app/client-index.html');
+// load routes
+var buy = require('./route/buy');
+app.use('/buy', buy);
+
+
+// buy route
+app.route('/buy')
+    .get(function(request, response){
+        response.sendFile(__dirname + '/app/client-index.html');
+        response.sendStatus(200);
+    });
+    .post(parseUrlencoded, function(request, response){
+                        
+    });
+
+// helpToBuy route
+var helpToBuyRoute = app.route('/helpToBuy')
+helpToBuyRoute.get(function(request, response){
+	response.sendFile(__dirname + '/app/')
+});
+helpToBuyRoute.post(function(request, response){
+	response.sendFile(__dirname + '/app/')
 });
 
-app.get('/', function(request, response){
+app.get('/myInfo', function(request, response){
 	
 });
 
 app.listen(3000, function(){
 	console.log('listening on port 3000');
 });
-
-// angular.module('NCKUBuyer', ['ngRoute']);
---
+++ server/route/buy.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(app){
+    app.get('/buy', function(request, response){
+        
+    });
+    
+    app.post('/buy', function(request, response){
+        
+    });
+};
