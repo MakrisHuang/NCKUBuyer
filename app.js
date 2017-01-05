5aabe2791ea99ada18fdc6db0af064a58223b3d5
Makris Huang <vallwesture@gmail.com>
2017-01-05 22:08:54 +0800

    connected to socket.io

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 // app initialization
 app.use(express.static('app'));
@@ -20,11 +18,16 @@ require('./server/routes/page_buy')(app);
 require('./server/routes/page_helpToBuy')(app);
 require('./server/routes/page_myInfo')(app);
 
+var server = app.listen(3000, function(){
+	console.log('listening on port 3000');
+});
+var io = require('socket.io')(server);
+
 // socket.io
 io.on('connection', function(socket){
     console.log('connected')
+    socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 })
 
-app.listen(3000, function(){
-	console.log('listening on port 3000');
-});
