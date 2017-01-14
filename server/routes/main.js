d95c652ab6781048f43ff54d82ff4d7f7b931345
Makris Huang <vallwesture@gmail.com>
2017-01-14 20:25:45 +0800

    fixed

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,5 +1,6 @@
 module.exports = function(app){
     app.get('/', function(request, response){
-        response.sendFile('client.html', {root: app.settings.app})   
+        response.sendFile('client.html', {root: app.settings.app})
+        console.log('client.html is sent')
     });
 };
